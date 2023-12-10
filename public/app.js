43239bc14bb1e007ea3d1ffb0e5107a2039b42db
Josh Rivera <beps@Mactop.local>
2023-12-10 09:53:47 -0600

    followed leaflet documentation to build a small polygon in paris surrounding the huxton

--- public/app.js
+++ public/app.js
@@ -1,13 +1,23 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
+var redPin = L.icon({
+    iconUrl: 'assets/red-pin.png',
+    iconSize: [38, 38],
+    iconAnchor: [19, 38],
+    popupAnchor: [0, -38]
+})
 
 
 
 // Create map
-
-
-
-
-// Add openstreetmap tiles
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
+// Add OpenStreetMap tiles:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
 
@@ -15,16 +25,29 @@
 
 
 // Create and add a geolocation marker
-
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<b>The Hoxton, Paris</b>').openPopup()
 
 
 
 // Draw the 2nd arrondissement
+let polygon = L.polygon([
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664]]).addTo(myMap)
 
 
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196], {icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739], {icon: redPin}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167], {icon: redPin}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543], {icon: redPin}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714], {icon: redPin}).bindPopup('Grands Boulevards')
 
-
-// Metro station markers
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
 
